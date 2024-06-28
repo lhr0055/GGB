0d9f7c464ad30fcde4e9e2acd79a46a1853231b1
Daeun22Chung <ddaeun8723@gmail.com>
2024-06-28 16:30:43 +0900

    Authorization with HOC
    관리자만 authorization 페이지 접근 가능

--- kimchi/src/HOC/index.ts
+++ kimchi/src/HOC/index.ts
@@ -1,3 +1,4 @@
 import withAuth from './withAuth';
+import withAdminAuth from "./withAdminAuth";
 
-export { withAuth };
+export { withAuth, withAdminAuth };
---
+++ kimchi/src/HOC/withAdminAuth.tsx
@@ -0,0 +1,36 @@
+import jwt_decode from "jwt-decode";
+import {SD_Roles} from "../Utility/SD";
+
+
+
+const withAdminAuth = (WrappedComponent : any) => {
+    return(props: any) => {
+        console.log("HOC called");
+        const accessToken = localStorage.getItem("token") ?? ""; // 토큰이 있는지 확인
+        if(accessToken) {
+            const decode: {
+                role: string;
+            } = jwt_decode(accessToken);
+
+
+            if(decode.role!==SD_Roles.ADMIN) {
+                window.location.replace("/accessDenied");
+                return null;
+
+            }
+
+            }
+
+        else { // 토큰이 없으면 login 화면으로 되돌리기
+            window.location.replace("/login");
+            return null;
+        }
+
+
+        return <WrappedComponent {...props}/>
+
+
+    }
+}
+
+export default withAdminAuth;
