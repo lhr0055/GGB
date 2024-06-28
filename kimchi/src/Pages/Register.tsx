63b32a12f80418582f1476da0d0d32e7745f3efb
Daeun22Chung <ddaeun8723@gmail.com>
2024-06-28 13:15:43 +0900

    Add Loader on Register
    회원가입 버튼 클릭 시 로딩 아이콘 구현

--- kimchi/src/Pages/Register.tsx
+++ kimchi/src/Pages/Register.tsx
@@ -4,6 +4,7 @@ import {inputHelper, toastNotify} from "../Helper";
 import {useRegisterUserMutation} from "../Apis/authApi";
 import {apiResponse} from "../Interfaces";
 import {useNavigate} from "react-router-dom";
+import {MainLoader} from "../Components/Page/Common";
 
 
 
@@ -48,6 +49,7 @@ function Register() {
 
     return (
         <div className="container text-center">
+            {loading && <MainLoader/>}
             <form method="post" onSubmit={handleSubmit}>
                 <h1 className="mt-5">Register</h1>
                 <div className="mt-5">
@@ -97,7 +99,7 @@ function Register() {
                     </div>
                 </div>
                 <div className="mt-5">
-                    <button type="submit" className="btn btn-success">
+                    <button type="submit" className="btn btn-success" disabled={loading}>
                         Register
                     </button>
                 </div>
