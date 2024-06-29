e4979856b0e1d74a4bc265cb2e98125cdfcdb6fa
Daeun22Chung <ddaeun8723@gmail.com>
2024-06-29 10:19:45 +0900

    Create Payment API
    Strip,  react Strip.js

--- kimchi/src/Apis/index.ts
+++ kimchi/src/Apis/index.ts
@@ -1,6 +1,7 @@
 import menuItemApi   from "./menuItemApi";
 import shoppingCartApi from "./shoppingCartApi"
 import authApi from "./authApi"
+import paymentApi from "./paymentApi";
 
 
-export {menuItemApi, shoppingCartApi, authApi};
+export {menuItemApi, shoppingCartApi, authApi, paymentApi};
