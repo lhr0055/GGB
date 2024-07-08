f0e3d412cdbf4f2f8ca13ee4a147266d63f8428a
lhr0055 <129835424+lhr0055@users.noreply.github.com>
2024-07-08 09:33:00 +0900

    Not Found Img Change etc....
    Not Found 이미지 변경 및 이슈 27,28 해결

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -4,10 +4,15 @@ function NotFound () {
     let img = require("../Assets/Images/not.png");
 
     return(
-        <div>   
-            <h3>NotFound</h3> 
-            <h1>Kimchi is an ingredient for cooking </h1>
-            <img src={img} alt="404" width="25%"/>  
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                margin:"50px"
+            }}>   
+            {/* <h3>NotFound</h3> 
+            <h1>Kimchi is an ingredient for cooking </h1> */}
+            <img src={img} alt="404" width="60%"/>  
         </div>
     )
 }
