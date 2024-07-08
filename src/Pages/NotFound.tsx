6ab1926bb99ebd047e190c93277f16f97e193f5a
lhr0055 <129835424+lhr0055@users.noreply.github.com>
2024-07-08 09:18:12 +0900

    notFoundImg

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 
 function NotFound () {
-    let gif = require("../Assets/Images/korea.gif");
+    let img = require("../Assets/Images/not.png");
 
     return(
         <div>   
             <h3>NotFound</h3> 
             <h1>Kimchi is an ingredient for cooking </h1>
-            <img src={gif} alt="404" width="25%"/>  
+            <img src={img} alt="404" width="25%"/>  
         </div>
     )
 }
