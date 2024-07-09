cee5fb527e344bae2403ff09492ce54027b79656
lhr0055 <129835424+lhr0055@users.noreply.github.com>
2024-07-09 13:20:23 +0900

    status edi\

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -7,11 +7,11 @@
         public const string Role_Customer = "customer";
 
 
-        public const string status_payment = "Payment";
-        public const string status_confirmed = "Confirmed";
-        public const string status_preparing = "Preparing";
-        public const string status_shipping = "Shipping";
-        public const string status_Completed = "Completed";
-        public const string status_Cancelled = "Cancelled";
+        public const string status_payment = "결제완료";
+        public const string status_confirmed = "주문확인";
+        public const string status_preparing = "상품준비중";
+        public const string status_shipping = "배송중";
+        public const string status_Completed = "배송완료";
+        public const string status_Cancelled = "취소";
     }
 }
