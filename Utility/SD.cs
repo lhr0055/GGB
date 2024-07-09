55d5453acaa4e1f3c0ac628a0f296d09e8ff5583
lhr0055 <129835424+lhr0055@users.noreply.github.com>
2024-07-09 13:08:19 +0900

    status change

--- Utility/SD.cs
+++ Utility/SD.cs
@@ -7,10 +7,10 @@
         public const string Role_Customer = "customer";
 
 
-        public const string status_pending = "Pending";
+        public const string status_payment = "Payment";
         public const string status_confirmed = "Confirmed";
-        public const string status_beingCooked = "Being Cooked";
-        public const string status_readyForPickUp = "Ready for Pickup";
+        public const string status_preparing = "Preparing";
+        public const string status_shipping = "Shipping";
         public const string status_Completed = "Completed";
         public const string status_Cancelled = "Cancelled";
     }
